058bc30e01014338d965718185ca0a00dd09acdf
Jonathan Rainville <jonathanrainville@status.im>
2018-04-16 17:04:59 -0400

    finish compile code and fix small errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Compiler = require("./lib/Compiler");
 
 module.exports = (embark) => {
-  embark.registerCompiler('bbo', compileBamboo);
+  embark.registerCompiler('.bbo', compileBamboo);
 
   function compileBamboo(contractFiles, cb) {
     if (!contractFiles || !contractFiles.length) {
